a7a2479ec3ba707a880015ed943e17a72349383e
Justin Dial <justinalandial@gmail.com>
2018-11-18 14:11:43 -0600

    hyvee

--- index.js
+++ index.js
@@ -29,23 +29,44 @@ app.post('/storeSearch', function(req, res) {
     }
     axios.get('https://redsky.target.com/v1/plp/search/?keyword='+req.body.productName)
     .then(function (response) {
-      console.log('COUNT: '+Object.keys(response.data.search_response.items.Item).length);
-      for (i in response.data.search_response.items.Item){
-        console.log("REQ PROD: "+requestedProduct);
-        console.log("PROD NAME: "+response.data.search_response.items.Item[i].title);
-        if(exactFlag && response.data.search_response.items.Item[i].title == requestedProduct){
-          console.log("----MATCH----");
-          products.push(response.data.search_response.items.Item[i]);
+      targetItems=response.data.search_response.items.Item;
+      console.log(targetItems.length);
+      //console.log('COUNT: '+Object.keys(response.data.search_response.items.Item).length);
+      for (i in targetItems){
+        //console.log("REQ PROD: "+requestedProduct);
+        //console.log("PROD NAME: "+response.data.search_response.items.Item[i].title);
+        targetItems[i].price=targetItems[i].offer_price.price;
+        targetItems[i].store="Target";
+        if(exactFlag && targetItems[i].title == requestedProduct){
+          //console.log("----MATCH----");
+          products.push(targetItems[i]);
           break;
         }
         else if(!exactFlag){
-          products.push(response.data.search_response.items.Item[i]);
+          products.push(targetItems[i]);
         }
       }
-      res.send(products);
-    })
-    .catch(function (error) {
-      console.log(error);
+      axios.get('https://www.hy-vee.com/grocery/search?search='+req.body.productName)
+        .then(function (response) {
+          //console.log(response.data);
+          indexStart=response.data.indexOf('Skip to main content</a></div>')+56;
+          indexEnd=indexStart+response.data.substring(indexStart,response.data.length).indexOf('window.dataLayer = window.dataLayer || [];');
+          // console.log("INDEX START: "+indexStart);
+          // console.log("INDEX END: "+indexEnd);
+          // console.log("LENGTH: "+response.data.length);
+          // console.log(response.data.substring(indexStart,indexEnd-49));
+          hyveeItems=response.data.substring(indexStart,indexEnd-49);
+          console.log(hyveeItems.length);
+          hyveeItems=eval(hyveeItems);
+          for (i in hyveeItems){
+            hyveeItems[i].title=hyveeItems[i].name;
+            hyveeItems[i].store="Hy-Vee";
+            console.log(hyveeItems[i].name);
+            console.log(hyveeItems[i].price);
+            products.push(hyveeItems[i]);
+          }
+          res.send(products);
+        })
     });
 });
 
