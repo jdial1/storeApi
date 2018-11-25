178cd05a59c22670ae5d51080b3818577a77e071
Justin Dial <justinalandial@gmail.com>
2018-11-25 16:16:17 -0600

    index

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const moment = require("moment");
 const path = require("path");
 const axios = require("axios");
+const _ = require("lodash");
+
 axios.defaults.headers.common['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36";
 axios.defaults.headers.common['Cookie'] = "build_sha=55cbe202166a90a014c379bc4a14c7fe3199d326; ahoy_visitor=2940d95b-499d-4873-bdeb-344f38b4062e; ajs_group_id=null; ajs_anonymous_id=%22bd705114-0cb9-4df1-a7a6-51b478f4b1d6%22; remember_user_token=W1s0MzE0NDA4MF0sIiQyYSQxMCRPU2xKbVBxUmoyOWRaNHJmZ29MNXRlIiwiMTU0MjU3OTU5Ni4zNjk4MzIiXQ%3D%3D--d333d0a4276b212859cb5ecb0d8eb684d47f35d3; ajs_user_id=43144080; ahoy_visit=6c061735-d466-4891-8483-f5062afae673; ahoy_track=true; _instacart_session=Q2oxcTR3TW5OWDNaN0k5ME5Ib1Q0R1V5T01WQjNqV21ndFhNemJrWlhCNEhIaDlMelU5OWlmZW8vUWtqRDlYc3JJT2huUHIvalh2b1R1WkNZS1cxZHJneHZTNEVvWHFJdmFZd2tzVGN1RVg5MWc5NUtSbXRyeFpDNUtJTHdQeEE1V0pjWnNMOUJ0elI4VGpLZkRHdnRjeEx4YkREcEtXMExPM1BUTFc0Q1Nuc0JMclhKZVN2ZEJWa2NJWmI4cGNOVTFNK0dlNE5GWjhLWXZoUUtXQWRaODc4Zk1GOHdkdG51Y3NuU21rUDhoRFBuNWFkR213Vm42M0NOQ0pRNnNzb2NWa0JTWjJ1M25YVnh6MUFLSFhQSlpVVTg0c1BWNnp4V3JsWTVVOEcwczFHQ0lSK3VxWWJvU0VNOWJVNEswR2JoaTVSaTFYUHhpbjg2SlltOXZUVkc2Z1hxYysyRXgxL2RMSWNOTUEzeGtXVmx2SWk2WlZqM1MzMFBZMEw1RVZhaVB3bjJYUkZ0ZE5TUFg0amZBeE5Gd1o2TW40cTcrbS9LK0QzSXVISjd4SkF0N3VzTGprbVN3RlRXbklvK0ZtNWZmM1RnRmwrV29Kbk1WN28yd1RNdWNPdGwraS9aamczMGo5blpWR2tycXc9LS02NVRQTUprTy9keVR2bVFiUWZkQUh3PT0%3D--bd52c13c4a933ba1390cacdb440115bb1536b728";
 
@@ -19,7 +21,7 @@ const resultsPerSite = 2;
 
 function getKeyifExist(object,Objkey) {
   console.log(object);
- if (object.hasOwnProperty(Objkey)){
+ if (_.has(object, Objkey)){
     return object[Objkey]
  }
  else {
